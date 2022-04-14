d686f1727db553d637a72dc991074e905255f3b4
Emilio Gril <emiliogril@gmail.com>
2022-04-13 23:06:25 -0300

    Entrega Proyecto Final

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -6,14 +6,14 @@ import { useParams } from "react-router-dom";
 
 
 const ItemListContainer = (props) => {
-    const [products, setProductsId] = useState([]);
+    const [products, setProducts] = useState([]);
 
-    const { setProducts: categoryId } = useParams();
+    const { categoryId } = useParams();
     
 
     useEffect (()=>{
       getProducts(categoryId).then(product =>{
-        setProductsId(product)
+        setProducts(product)
         
       })
   },[categoryId])
